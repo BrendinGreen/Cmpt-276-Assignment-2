44baef96dc6ceabcb67c6fe9743d52e7565c044d
Brendin Green <brending@sfu.ca>
2017-09-29 08:08:36 -0700

    Better code structure

--- public/calculate.js
+++ public/calculate.js
@@ -6,21 +6,8 @@ var grades = [65.95, 56.98, 78.62, 96.1, 90.3, 72.24, 92.34, 60.00, 81.43, 86.22
 function calculateValues(){
 
     // Holds all of the threshold values
-    var thresholds = [];
-    // Sets up the threshold values via DOM
-    for (var i = 0; i < sections; i++){
-        thresholds[i] = parseFloat(document.getElementById("threshold"+i).value);
-    }
-
-    var errorFlag = false;
-    for (var n = 0; n < sections-1; n++){
-        if (thresholds[n] >= thresholds[n+1]){
-            document.getElementById("threshold"+n).style.color = "red";
-            errorFlag = true;
-        } else {
-            document.getElementById("threshold"+n).style.color = "black";
-        }
-    }
+    var thresholds = getThresholds()
+    var errorFlag = validation(thresholds);
 
     // Holds the number of steps the histogram will display for each category
     var numberOfStudents = new Array(sections-1).fill(0);
@@ -33,16 +20,40 @@ function calculateValues(){
         }
     }
 
-    // Set DOM objects
-    for (var k = 0; k < numberOfStudents.length; k++){
-
-        document.getElementById("result" + k + "progress").value = (errorFlag) ? 0 : numberOfStudents[k];
-        document.getElementById("result" + k + "progress").max = "" + numberOfStudents.reduce(function (p1, p2) { return Math.max(p1, p2) }) ;
-    }
-
+    setProgress(errorFlag, numberOfStudents);
 }
 
+// Sets the progress of all of the histogram bars
+function setProgress(errorFlag, dataArray){
+    // Set DOM objects
+    for (var k = 0; k < dataArray.length; k++){
+        document.getElementById("result" + k + "progress").value = (errorFlag) ? 0 : dataArray[k];
+        document.getElementById("result" + k + "progress").max = "" + dataArray.reduce(function (p1, p2) { return Math.max(p1, p2) }) ;
+    }
+}
 
+// Gets all user entered data
+function getThresholds(){
+    // Holds all of the threshold values
+    var thresholds = [];
+    // Sets up the threshold values via DOM
+    for (var i = 0; i < sections; i++){ thresholds[i] = parseFloat(document.getElementById("threshold"+i).value); }
+    return thresholds;
+}
+
+// Checks that thresholds are valid (ex. are in sorted order) and highlights any errors
+function validation(thresholds){
+    var errorFlag = false;
+    for (var n = 0; n < sections-1; n++){
+        if (thresholds[n] >= thresholds[n+1]){
+            document.getElementById("threshold"+n).style.color = "red";
+            errorFlag = true;
+        } else {
+            document.getElementById("threshold"+n).style.color = "black";
+        }
+    }
+    return errorFlag;
+}
 
 // Forces input into 2 decimal place format
 function formatValues(input) {
